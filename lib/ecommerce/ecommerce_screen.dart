e21755ef7d6f8d18ac8233b64559e4927be11108
Fadhil Muhammad <famuh79@gmail.com>
2022-04-22 15:42:03 +0700

    progress 1

--- lib/ecommerce/ecommerce_screen.dart
+++ lib/ecommerce/ecommerce_screen.dart
@@ -1,5 +1,5 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-
 
 class ecommerceScreen extends StatefulWidget {
   ecommerceScreen({Key? key}) : super(key: key);
@@ -9,21 +9,51 @@ class ecommerceScreen extends StatefulWidget {
 }
 
 class _ecommerceScreenState extends State<ecommerceScreen> {
+  int _currentIndex = 0;
+  List _navigation = [
+    //home
+
+    //favorite
+
+    //notification
+
+    //profile
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  bottomNavigationBar: CurvedNavigationBar(
-    backgroundColor: Colors.blueAccent,
-    items: <Widget>[
-      Icon(Icons.add, size: 30),
-      Icon(Icons.list, size: 30),
-      Icon(Icons.compare_arrows, size: 30),
-    ],
-    onTap: (index) {
-      //Handle button tap
-    },
-  ),
-  body: Container(color: Colors.blueAccent),
-);
+      appBar: AppBar(
+        backgroundColor: Colors.grey[400],
+        leading: IconButton(
+          icon: Icon(Icons.person),
+          onPressed: (){
+            print('Leading Pressed !');
+          },
+        ),
+        title: Image.asset('../assets/logo.png', width: 100,),
+        ),
+
+      body: Container(color: Colors.white30),
+
+      //bottom Navbar
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.green,
+        items: <Widget>[
+          Icon(Icons.home, size: 30),
+          Icon(Icons.favorite, size: 30),
+          Icon(Icons.notifications, size: 30),
+          Icon(Icons.person, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+          setState(() {
+            _currentIndex = index;
+            print('Current Index : ' + index.toString());
+          });
+        },
+      ),
+      
+    );
   }
 }
